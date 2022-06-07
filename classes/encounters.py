64ae142b4bbced7f00fad397daee20ef1f0099dd
Shades <patrick4355@gmail.com>
2022-06-07 01:39:05 -0400

    started encounters lists

--- classes/encounters.py
+++ classes/encounters.py
@@ -5,16 +5,38 @@ def random_encounter(num_combatants: int, player: object):
     encounters = []
 
     if player.level <= 3:
-        encounters = [shop, goblinEncounter, wolfEncounter]
+        encounters = [shop, goblin_encounter, wolf_encounter]
     elif player.level <= 6:
-        encounters = [shop, goblinEncounter, wolfEncounter,
-                      undeadEncounter, zombieHorde]
+        encounters = [shop, goblin_encounter, wolf_encounter,
+                      undead_encounter, zombie_encounter]
     else:
-        encounters = [shop, goblinEncounter, wolfEncounter,
-                      undeadEncounter, zombieHorde, vampireEncounter]
+        encounters = [shop, goblin_encounter, wolf_encounter,
+                      undead_encounter, zombie_encounter, vampire_encounter]
 
     randomChoice = random.choices(encounters)[0]
 
     return randomChoice(num_combatants)
 
 
+def shop(num_of_foes: int):
+    pass
+
+
+def goblin_encounter(num_of_foes: int):
+    pass
+
+
+def wolf_encounter(num_of_foes: int):
+    pass
+
+
+def undead_encounter(num_of_foes: int):
+    pass
+
+
+def zombie_encounter(num_of_foes: int):
+    pass
+
+
+def vampire_encounter(num_of_foes: int):
+    pass
