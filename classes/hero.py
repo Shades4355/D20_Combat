4720c08c7defeeed6a0e583f6fcdc6b732c2c769
Shades <patrick4355@gmail.com>
2022-06-09 13:13:10 -0400

    added space between Pick_Class and Hero

--- classes/hero.py
+++ classes/hero.py
@@ -26,6 +26,7 @@ class Pick_Class:
             print("Class Picking Error")
             exit(1)
 
+
 class Hero:
     """basic player hero class"""
     def __init__(self, name="Hero", class_name="Hero", base_health=10, xp=0, weapon=w.Unarmed(), inventory=[], armor=0, special=[], str=10, dex=10, con=10, int=10, wis=10, cha=10, gold=0):
