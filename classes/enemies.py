6cd2f64f48b2af3b22f40d4844f08ac2b922bd65
Shades <patrick4355@gmail.com>
2022-06-18 10:39:46 -0400

    readability change

--- classes/enemies.py
+++ classes/enemies.py
@@ -118,7 +118,7 @@ class Zombie(Undead):
                 self.name, hurt, self.current_hit_points))
         else:
             revival_chance = dice.roll(1, 100)
-            if revival_chance < 11:
+            if revival_chance <= 10:
                 self.current_hit_points = self.max_hit_points
                 print("{0.name} got back up".format(self))
             else:
