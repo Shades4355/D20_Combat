b4b8d4daa5ed00ead715ba091c1b3b554f8ac263
Shades <patrick4355@gmail.com>
2022-06-26 12:11:50 -0400

    updated testing file with new shop logic from main file

--- testing.py
+++ testing.py
@@ -26,36 +26,30 @@ input("[Enter]")
 print()
 
 hero.gold = 10
-e.listen_check(hero)
-for i in range(1):
-    e.shop(hero)
-
 # begin dungeon crawl
+encounters = ""
 while hero.alive:
     # pick number of enemies
     num_combatants = dice.roll(1, math.ceil(hero.class_level/2))
 
-    # pick random encounter (fight or shop)
-    if dice.roll(1, 4) > 1:
-        enemies_in_fight = e.random_encounter(num_combatants, hero)
-        hero.in_fight = True
-        
-        #print player health and level
-        time.sleep(1)
-        print("\n### New Encounter ###")
-        print("\n{0.name}\nHealth: {0.health}\nLevel: {0.class_level}".format(hero))
-        time.sleep(1)
+    # pick random encounter (fight or shop or skill check)
+    chance = dice.roll(1, 6)
+    if chance > 1 or encounters == "shop":
+        if encounters == "shop":  # skill check encounter
+            encounters = "skill"
+            e.skill_encounter(hero)
 
-        while hero.in_fight and hero.alive:
-            c.player_turn(hero, enemies_in_fight)
-            if hero.cooldown > 0:
-                hero.cooldown -= 1
-            c.enemy_turn(hero, enemies_in_fight)
-            if len(enemies_in_fight) <= 0:
-              hero.in_fight = False
+        elif chance in [2, 3, 4]:
+            encounters = "fight"
+            c.fight(hero, num_combatants)
+
+        elif chance in [5, 6]:  # skill check encounter
+            encounters = "skill"
+            e.skill_encounter(hero)
+
     else:
+        encounters = "shop"
         e.shop(hero)
-        hero.cooldown = 0
 
     # heal between fights
     heal = math.floor(hero.max_health/2)
