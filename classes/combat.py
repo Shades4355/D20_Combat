c6b1f98240878607d37da3c99f31c6cb9c8e2476
Shades <patrick4355@gmail.com>
2022-07-08 19:59:31 -0400

    Added fail message to enemy targeting

--- classes/combat.py
+++ classes/combat.py
@@ -200,7 +200,8 @@ def player_turn(player:object, enemies_in_fight:list):
       try:
           target = int(target)
       except:
-          target = 0
+        print("Pick a number between 1 and {}".format(len(enemies_in_fight)))
+        target = 0
 
     enemy = enemies_in_fight[target - 1]
 
